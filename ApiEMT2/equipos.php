79c06874c190c9e41d4ffb36d0259a80119ffeff
rafa <rafa@Daw>
2018-01-25 22:16:40 +0100

    Merge origin/master
    Conflicts:
    	src/main/java/utils/UrlsPaths.java

--- ApiEMT2/equipos.php
+++ ApiEMT2/equipos.php
@@ -1,62 +1,34 @@
-<?php
+ <?php
 
 require 'vendor/autoload.php';
-use GuzzleHttp\Client;  
-$client = new Client();
-
-$uri = 'http://api.football-data.org/v1/competitions/455/leagueTable';
-$header = array('headers' => array('X-Auth-Token' => '2deee83e549c4a6e9709871d0fd58a0a'));
-$response = $client->get($uri, $header);          
-$equipos = json_decode($response->getBody()); 
-var_dump($json);
-     foreach ( $json->standing as $team)
-    {
-        echo $team->teamName . " ". $team->points ." <br>";
-        
+    use GuzzleHttp\Client;     
+    $client2 = new Client();
+    $uri2 = 'http://api.football-data.org/v1/competitions/455/leagueTable';
+    $header2 = array('headers' => array('X-Auth-Token' => '7455d306d1734284a744a80bfe86e426'));
+    $response2 = $client2->get($uri2, $header2);          
+    $json2 = json_decode($response2->getBody());  
+    echo '<h5>' . $json2->leagueCaption . '</h5>';
+    echo '<table border=1>'
+    . '<tr>'
+    . '<th>Posicion</th>'
+    . '<th>Nombre del equipo</th>'
+    . '<th>Logo</th>'        
+    . '<th>Puntos</th>'
+    . '<th>Goles</th>'
+    . '<th>Goles en contra </th>'
+    . '<th>Diferencia de goles </th>'
+    //. '<th></th>'
+    . '</tr>';
+    foreach ($json2->standing as $liga) {
+        echo '<tr>';
+        echo '<td>' . $liga->position . '</td>';
+        echo '<td>' . $liga->teamName . '</td>';
+        echo '<td><img src="' . $liga->crestURI . '" width="60" height="80""></td>';
+        echo '<td>' . $liga->points . '</td>';
+        echo '<td>' . $liga->goals . '</td>';
+        echo '<td>' . $liga->goalsAgainst . '</td>';
+        echo '<td>' . $liga->goalDifference . '</td>';   
+        echo '</tr>';
     }
-?>
-
-<!DOCTYPE html>
-<html>
-<head>
-    <title></title>
-</head>
-<body>
-    <h1 align="center">LISTADO DE MEDICOS</h1>
-    <table width="70%" border="1px" align="center">
-        <?php
-        foreach ($equipos -> standing as equipo) {
-        ?>
-        <tr align="center">
-        <td>Codigo</td>
-        <td>Identificacion</td>
-        <td>Nombre</td>
-        <td>Apellidos</td>
-        <td>Especialidad</td>
-        <td>Telefono</td>
-        <td>Correo</td>
-    </tr>
-        <?php
-        }
-        ?>
-    
-    <?php 
-        while($datos=$resultado->fetch_array()){
-        ?>
-            <tr>
-                <td><?php echo $datos["idMedico"]?></td>
-                <td><?php echo $datos["medIdentificacion"]?></td>
-                <td><?php echo $datos["medNombres"]?></td>
-                <td><?php echo $datos["medApellidos"]?></td>
-                <td><?php echo $datos["medEspecialidad"]?></td>
-                <td><?php echo $datos["medTelefono"]?></td>
-                <td><?php echo $datos["medCorreo"]?></td>
-            </tr>
-            <?php   
-        }
-
-     ?>
-    </table>
-
-</body>
-</html>
+    echo '</table>';
+    echo "<br/> <br/> <br/> <br/>";
--- ApiEMT2/ligas.php
+++ ApiEMT2/ligas.php
@@ -3,8 +3,28 @@
 require 'vendor/autoload.php';
     use GuzzleHttp\Client;  
     $client = new Client();
-
-    $uri = 'http://api.football-data.org/v1/competitions/455/leagueTable';
-    $header = array('headers' => array('X-Auth-Token' => '2deee83e549c4a6e9709871d0fd58a0a'));
+    $uri = 'http://api.football-data.org/v1/competitions/?season=2017';
+    $header = array('headers' => array('X-Auth-Token' => '7455d306d1734284a744a80bfe86e426'));
     $response = $client->get($uri, $header);          
-    $json = json_decode($response->getBody()); 
+    $json = json_decode($response->getBody());  
+    
+    echo '<table border=1>'
+    . '<tr>'
+    . '<th>ID</th>'
+    . '<th>Nombre de la liga</th>'
+    . '<th>Liga</th>'
+    . '<th>Año</th>'
+    . '<th>Número de equipos</th>'
+    //. '<th></th>'
+    . '</tr>';
+    foreach ($json as $ligas) {
+        echo '<tr>';
+        echo '<td>' . $ligas->id . '</td>';
+        echo '<td>' . $ligas->caption . '</td>';
+        echo '<td>' . $ligas->league . '</td>';
+        echo '<td>' . $ligas->year . '</td>';
+        echo '<td>' . $ligas->numberOfTeams . '</td>';
+        echo '</tr>';
+    }
+    echo '</table>';
+    echo "<br/> <br/> <br/> <br/>";
